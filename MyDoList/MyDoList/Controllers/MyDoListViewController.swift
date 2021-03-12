1c27d27741d930f68b1537da4de94c04f6147b62
Bruno Fonseca Alves <76008140+brunofonsecaalves@users.noreply.github.com>
2021-03-12 15:08:51 -0300

    Upload

---
+++ MyDoList/MyDoList/Controllers/MyDoListViewController.swift
@@ -0,0 +1,105 @@
+//
+//  MyDoListViewController.swift
+//  MyDoList
+//
+//  Created by Bruno Alves on 26/02/21.
+//
+
+import UIKit
+import CoreData
+
+class MyDoListViewController: UITableViewController {
+
+    
+    var itemArray = [Item]()
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        loadItems()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyDoCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            (action) in
+
+            let newItem = Item(context: self.context)
+            
+            newItem.title = textField.text!
+            
+            newItem.done = false
+            
+            self.itemArray.append(newItem)
+           
+            self.saveItems()
+            
+        }
+        
+        alert.addTextField{(alertTextField) in alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        
+        
+        do {
+            try context.save()
+        }catch{
+            print("Error save context \(error)")
+        }
+        
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+            do {
+                itemArray = try context.fetch(request)
+            }catch{
+                print("Error fetching data \(error)")
+            }
+        }
+
+    }
+
+
